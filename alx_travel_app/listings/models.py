08619f1319d19cffe20ae4017edee0a0d800d692
Fernandes Mukanda <fernandezmukanda@gmail.com>
2025-10-19 19:30:40 +0300

    Update models.py

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-
-from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils import timezone
+import uuid
 
 
 class Listing(models.Model):
@@ -190,5 +189,6 @@ class Payment(models.Model):
         # Update booking status
         self.booking.status = 'confirmed'
         self.booking.save()
+
 
 
